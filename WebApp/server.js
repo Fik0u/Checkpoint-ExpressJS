c9c409708b455e28707a17afe5b847b37b357950
Fik0u <fikouch@gmail.com>
2025-02-09 21:03:50 +0100

    Checkpoint ExpressJS

--- WebApp/server.js
+++ WebApp/server.js
@@ -4,13 +4,17 @@ const app = express()
 
 require("dotenv").config()
 
+// On fait appel au template engine EJS
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 
+// La fonction de notre middleware
 const checkin = require("./middlewares/checkin.js")
 // app.use(checkin)
 
 
+
+// Nos routes
 app.get("/accueil", (req, res) => {
     res.render("accueil")
 })
@@ -24,8 +28,12 @@ app.get("/contact", (req, res) => {
 })
 
 
+
+//Importation du port
 PORT = process.env.PORT;
 
+
+// Le serveur sur écoute
 app.listen(PORT, (err) => {
     err ? console.log(err) 
         : console.log(`Server is running on port ${PORT}`)
